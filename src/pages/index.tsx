317a125c118cd596eeadf01748257e3af94f7501
Silvio Melo Filho <smfilho@gmail.com>
2021-04-17 19:40:25 -0500

    Using GetStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import styles from './home.module.scss';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IgMRyAXgq7dw1SXoCkrO5Lv');
 
   const product = {
@@ -50,5 +50,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24hrs
   };
 };
